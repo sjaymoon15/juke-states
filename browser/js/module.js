1cc6fb3c4c18a5a425478105557bd9cb4b16e711
Seungjae Moon <sjaymoon15@gmail.com>
2016-07-07 17:07:54 -0400

    artist done

--- browser/js/module.js
+++ browser/js/module.js
@@ -1,3 +1,14 @@
 'use strict';
 
-var juke = angular.module('juke', []);
+var juke = angular.module('juke', ['ui.router'])
+	.config(function ($stateProvider) {
+	  $stateProvider.state('albumList', {
+	    url: '/albums',
+	    templateUrl: '/albums.html' 
+	  })
+	  .state('artistList',{
+	  	url: '/artists',
+	  	templateUrl: '/artists.html'
+	  });
+	});
+
