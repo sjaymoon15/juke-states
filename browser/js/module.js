ca8d43d715b4f05c8ff9940277f1259b027e9250
Jonathan Ligh <JonathankLigh@gmail.com>
2016-07-07 18:09:31 -0400

    works but broken

--- browser/js/module.js
+++ browser/js/module.js
@@ -6,9 +6,29 @@ var juke = angular.module('juke', ['ui.router'])
 	    url: '/albums',
 	    templateUrl: '/albums.html' 
 	  })
+	  .state('album', {
+	    url: '/albums/:albumId',
+	    templateUrl: '/album.html',
+	    parent: 'albumList'
+	  })
 	  .state('artistList',{
 	  	url: '/artists',
-	  	templateUrl: '/artists.html'
+	  	templateUrl: 'artists.html'
+	  })
+	  .state('artistList.artist', {
+	  	url: '/artist/:artistId',
+	  	templateUrl: '/artist.html',
+	  	parent: 'artistList'
+	  })
+	  .state('artistList.artist.songs', {
+	  	url: '/artist/:artistId/songs',
+	  	templateUrl: '/artist.songs.html',
+	  	parent: 'artistList.artist'
+	  })
+	  .state('artistList.artist.albums', {
+	  	url: '/artist/:artistId/albums',
+	  	templateUrl: '/artist.albums.html',
+	  	parent: 'artistList.artist'
 	  });
 	});
 
